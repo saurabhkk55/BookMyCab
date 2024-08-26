4f90ee39aa129fde4d2dacd59d63077b24ad51e9
saurabhkk55 <saurabhkumarkardam25@gmail.com>
2024-08-26 14:21:30 +0530

    enabled Swagger UI feature

--- uberApllication/src/main/java/com/saurabhorg/uber/uberApllication/controllers/RiderController.java
+++ uberApllication/src/main/java/com/saurabhorg/uber/uberApllication/controllers/RiderController.java
@@ -5,6 +5,7 @@ import com.saurabhorg.uber.uberApllication.services.RiderService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,8 +31,16 @@ public class RiderController {
         return ResponseEntity.ok(riderService.getMyProfile());
     }
 
-    @GetMapping("/getAllMyRides")
-    public ResponseEntity<Page<RideDTO>> getAllMyRides(PageRequest pageRequest) {
+//    @GetMapping("/getAllMyRides")
+//    public ResponseEntity<Page<RideDTO>> getAllMyRides(PageRequest pageRequest) {
+//        return ResponseEntity.ok(riderService.getAllMyRides(pageRequest));
+//    }
+
+    @GetMapping("/getMyRides")
+    public ResponseEntity<Page<RideDTO>> getAllMyRides(@RequestParam(defaultValue = "0") Integer pageOffset,
+                                                       @RequestParam(defaultValue = "10", required = false) Integer pageSize) {
+        PageRequest pageRequest = PageRequest.of(pageOffset, pageSize,
+                Sort.by(Sort.Direction.DESC, "createdTime", "id"));
         return ResponseEntity.ok(riderService.getAllMyRides(pageRequest));
     }
 
